b705d1d15e00e5a5caa6f599869ccb8dc36a20dc
kevinMacPro <cke8671@hotmail.com>
2016-01-09 21:40:20 +0900

    running version

--- tractography_rerun_OFC.py
+++ tractography_rerun_OFC.py
@@ -21,10 +21,8 @@ def tractography(args):
     dataLoc = '/Volumes/CCNC_3T_2/kcho/ccnc/GHR_project'
     subject_list = args.subjects
 
-    '''
-    Make exclusion mask in order to exclude tracks
-    going towards posterior paths from the thalamus
-    '''
+    # Make exclusion mask in order to exclude tracks
+    # going towards posterior paths from the thalamus
     for subject in subject_list:
         for side in ['lh', 'rh']:
             roiLoc = os.path.join(dataLoc, subject, 'ROI')
@@ -35,7 +33,7 @@ def tractography(args):
                 thalamusPosterior(thalamusROI)
 
 
-    '''Dictionary for datasource'''
+    # Dictionary for datasource
     info = dict(dwi=[['subject_id', 'data']],
                 bvecs=[['subject_id', 'bvecs']],
                 bvals=[['subject_id', 'bvals']],
@@ -44,7 +42,7 @@ def tractography(args):
                 exclusion_masks=[['subject_id', 
                     ['LPFC', 'LTC', 'MPFC','MTC',
                      'OCC','OFC','PC','SMC']]],
-                posterior_em = [['subject_id', '_post_thal_excl_mask.nii.gz']],
+                posterior_em = [['subject_id', 'post_thal_excl_mask']],
                 thsample = [['subject_id',
                     ['merged_th1samples','merged_th2samples']]],
                 phsample = [['subject_id',
@@ -58,13 +56,13 @@ def tractography(args):
                 aparc_aseg= [['subject_id','aparc+aseg']],
                 )
 
-    '''Subject name list setting into identity interface'''
+    # Subject name list setting into identity interface
     infosource = pe.Node(interface=util.IdentityInterface(fields=['subject_id']),
                          name="infosource")
     infosource.iterables = ('subject_id', subject_list)
 
 
-    '''Data source setting'''
+    # Data source setting
     datasource = pe.Node(interface=nio.DataGrabber(infields=['subject_id'],
                                                    outfields=info.keys()),
                          name = 'datasource')
@@ -92,7 +90,6 @@ def tractography(args):
     brainStemExtract = pe.Node(interface=fs.Binarize(out_type='nii.gz'), name='brainStem')
     brainStemExtract.inputs.match = [16, 6, 7, 8, 45, 46, 47]
 
-
     wmExtract = pe.Node(interface=fs.Binarize(out_type='nii.gz'), name='wmExtract')
     #    2   Left-Cerebral-White-Matter              245 245 245 0
     #    41  Right-Cerebral-White-Matter             0   225 0   0
@@ -107,14 +104,14 @@ def tractography(args):
     # - brain stem
     # - contralateral hemisphere white matter 
     # - slice posterior to the thalamus 
-    bs_add_wm = pe.Node(interface=fsl.MultiImageMaths(), name='add_masks')
+    bs_add_wm = pe.Node(interface=fsl.MultiImageMaths(), name='initialMask')
     bs_add_wm.inputs.op_string = '-add %s'
 
-    bs_add_wm_add_ex = pe.Node(interface=fsl.MultiImageMaths(), name='add_masks')
+    bs_add_wm_add_ex = pe.Node(interface=fsl.MultiImageMaths(), name='exclusionMask')
     bs_add_wm_add_ex.inputs.op_string = '-add %s'
 
 
-    '''Probabilistic tractography'''
+    # Probabilistic tractography
     probtrackx = pe.Node(interface=fsl.ProbTrackX2(), name='probtrackx_OFC')
     probtrackx.inputs.onewaycondition= True
     #Below are the default options
@@ -128,13 +125,13 @@ def tractography(args):
     #probtrackx.inputs.loop_check = True
 
 
-    '''Data sink'''
+    # Data sink
     datasink = pe.Node(interface=nio.DataSink(),name='datasink')
     datasink.inputs.base_directory = os.path.abspath('/Volumes/CCNC_3T_2/kcho/ccnc/GHR_project/OFC_tractography_wm')
     datasink.inputs.substitutions = [('_variable', 'variable'),
                                      ('_subject_id_', 'subject_id')]
 
-    ''' Workflow '''
+    # Workflow 
     dwiproc = pe.Workflow(name="dwiproc_OFC")
     dwiproc.base_dir = os.path.abspath('tractography_OFC_wm_pthalex')
     dwiproc.connect([
@@ -143,9 +140,10 @@ def tractography(args):
                         (datasource, wmExtract, [('aseg', 'in_file')]),
                         (brainStemExtract, bs_add_wm,[('binary_file', 'in_file')]),
                         (wmExtract, bs_add_wm,[('binary_file', 'operand_files')]),
-                        (bs_add_wm, bs_add_wm_add_ex, [('out_file', 'in_file')]),
                         (datasource, bs_add_wm_add_ex, [('posterior_em', 'operand_files')]),
+                        (bs_add_wm, bs_add_wm_add_ex, [('out_file', 'in_file')]),
                         (bs_add_wm_add_ex, probtrackx,[('out_file', 'avoid_mp')]),
+                        (bs_add_wm_add_ex,datasink,[('out_file','exclusion_mask')]),
                         (datasource,probtrackx,[('seed_file','seed'),
                                                    ('target_mask','stop_mask'),
                                                    ('target_mask','waypoints'),
@@ -159,12 +157,11 @@ def tractography(args):
                             ('log', 'probtrackx.@log'),
                             ('particle_files', 'probtrackx.@particle_files'),
                             ('targets', 'probtrackx.@targets'),
-                            ('way_total', 'probtrackx.@way_total')
+                            ('way_total', 'probtrackx.@way_total'),
                             ])
-                        (bs_add_wm_add_ex,datasink,[('out_file','exclusion_mask')]),
                     ])
 
-    '''Parallel processing'''
+    # Parallel processing
     dwiproc.run(plugin='MultiProc', plugin_args={'n_procs' : 8})
 
 
@@ -184,9 +181,6 @@ def get_opposite(roiList):
                 newList.append(i)
         print newList
     return newList
-
-
-
 
 def thalamusPosterior(thalamusImg):
     roiLoc = os.path.dirname(thalamusImg)
